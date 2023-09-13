8e395c29ce43c1818ffc53e49b792538dac27cca
mokarrom17 <mokarrombasher@gmail.com>
2023-09-13 14:05:46 +0600

    after proptypes

---
+++ src/Components/Blogs/Blogs.jsx
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+const Blogs = () => {
+    const[blogs,setBlogs] = useState([]);
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res => res.json())
+        .then(data => setBlogs(data))
+    },[])
+    return (
+        <div className="w-2/3 text-3xl font-semibold">
+            <h2>Blogs: {blogs.length}</h2>
+        </div>
+    );
+};
+
+export default Blogs;
---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import profile from '../../assets/images/profile.png'
+const Header = () => {
+    return (
+        <div className='flex justify-between items-center p-4 mx-5 border-b-2'>
+            <h1 className='text-4xl font-bold'>knowledge Cafe</h1>
+            <img src={profile} alt="" />
+
+        </div>
+    );
+};
+
+export default Header;
